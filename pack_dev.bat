3b1bfabf1e10038ab17a10131fac9a7da497391f
Saint Wish <SaintWish@users.noreply.github.com>
2022-10-06 18:10:47 -0600

    closes #84 (#85)

--- pack_dev.bat
+++ pack_dev.bat
@@ -6,28 +6,19 @@
 :loop
 title Packing TEST SC.DLL %date% %time%
 echo Packing not-safe for client test patch
-echo Got argument %1
 echo.
-if "%1"=="" goto :error_par
 cd .\scripts
 echo Setting time stamp.
 echo { >beta_date.script
 echo 	const BETA_TIMESTAMP "%date%" >>beta_date.script
-echo 	setvarg G_SCRIPT_VERSION %1% >>beta_date.script
 echo } >>beta_date.script
 echo Compiling...
 "../ScriptPack.exe"
 move sc.dll "..\"
-cd..
+cd ..
 echo completed %date% %time%
 
 goto :end
-
-:error_par
-echo NO PARAMTERS!
-echo.
-echo This batch file requires 7 character G_SCRIPT_VERSION param ( eg. JUN2007 )
-pause
 
 :end
 pause
